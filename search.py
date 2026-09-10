3a1cc76a5cedd6365bfaec5fe33ed592f1548933
DrizzyDrop24 <sahara.bangura001@albright.edu>
2026-09-09 21:49:09 -0400

    Update search.py

--- search.py
+++ search.py
@@ -1,18 +1,33 @@
 # search.py
 # Implementations of simple searching algorithms
 # Starter Code from CSC 210
-# Modified by: [Your Name]
+# Modified by: [Sahara Bangura]
 
 # This version of linear search looks for
 # *key* in *lst* and returns a boolean (True or False)
 # depending on if it found it
 def linear_search(lst, key):
-    # YOUR CODE HERE
+    for item in 1st:
+        if item == key:
+            return True
+    return False
     pass
 
 # This version of binary search looks for
 # *key* in *lst* and returns a boolean (True or False)
 # depending on if it found it
 def binary_search(data, key):
-    # YOUR CODE HERE
+    low = 0
+    high = len(data) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if data[mid] == key: 
+            return True
+        elif data[mid] < key:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return False
     pass
