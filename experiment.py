22586d834b2dda0c89fc9d4be35d142925e3e512
DrizzyDrop24 <sahara.bangura001@albright.edu>
2026-09-09 22:12:14 -0400

    Update experiment.py

--- experiment.py
+++ experiment.py
@@ -1,7 +1,7 @@
 # experiment.py
 # Run time trials of searching algorithms
 # Starter Code from CSC 210
-# Modified by: [Your Name]
+# Modified by: [Sahara Bangura]
 
 from random import randint
 from search import linear_search, binary_search
@@ -11,7 +11,45 @@ from time import perf_counter_ns
 # every integer between *min_value* and *max_value*
 # of length, *length*
 def random_int_list(length, min_value, max_value):
-    # YOUR CODE HERE
+    result[]
+
+    for _ in range(length):
+        result.append(randint(min_value, max_value))
+
+    return result
+
+# Conduct timing trials
+def time_trial(num_trials, data_size, min_value, max_value))
+
+    data = random_int_list(data_size, min_value, max_value)
+
+    # Time sorting once
+    sort_start = perf_counter_ns()
+    sorted_data = sorted(data)
+    sort_end = perf_counter_ns()
+
+    sort_time = sort_end - sort_start
+
+    linear_total = 0
+    binary_total = sort_time #include sorting cost
+
+    for _ in range(num trials):
+        key = randint(min_value, max_value)
+
+        start = perf_counter_ns()
+        linear_search(data, key)
+        end = perf_counter_ns()
+        linear_total += (end - start)
+
+        start= perf_counter_ns()
+        binary_search(sorted_data, key)
+        end = perf_counter_ns()
+        binary_total += (end - start)
+
+    avg_linear = linear_total / num_trials
+    avg_binary = binary_total / num_trials
+
+    return(float(avg_linear), float(avg_binary))
     pass
     
 # Conduct *num_trials* number of trials of linear and
